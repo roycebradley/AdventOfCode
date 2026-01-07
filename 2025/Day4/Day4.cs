f6e2eb4e69263ad274c8bb29239fa5dc34dcc551
roycebradley <broyce18@yahoo.com>
2026-01-06 19:25:24 -0500

    Day 4 Part2

--- 2025/Day4/Day4.cs
+++ 2025/Day4/Day4.cs
@@ -65,4 +65,109 @@ class Day4
 
         Console.WriteLine(finalCount);
     }
+
+
+    public static List<(int,int)> ToRemove(char[][] grid)
+    {
+       //char[][] grid = Day4.GetInputFile();
+
+        int finalCount = 0;
+        List<(int, int)> rcToRemove = new List<(int, int)>();
+
+        for(int r = 0; r < grid.Length; r++)
+        {
+            for(int c = 0; c < grid[r].Length; c++)
+            {
+                //if value == @
+                //nested loop that spans 3 x 3
+                //if count is less than 4 than we add to total count
+
+                if(grid[r][c] == '@')
+                {
+
+                    int count = 0;
+
+                    for(int row = -1; row <= 1; row ++)
+                    {
+                        for(int col = -1;  col <= 1; col++)
+                        {
+                            if(r + row < 0 || c + col < 0 || r + row >= grid.Length || c + col >= grid[r].Length)
+                            {
+                              continue;  
+                            } 
+                            
+                            if((grid[r + row][c + col] == '@') && !(row == 0 && col == 0))
+                            {
+                                count++;
+                    
+                            }
+                        }
+                    }
+                
+                    if(count < 4)
+                    {
+                    
+                    finalCount++;
+                    //remove accessible roll 
+                    rcToRemove.Add((r,c));
+                    }
+            
+                }
+            }
+        }
+
+       return rcToRemove;
+    }
+
+
+    private static char[][] RemoveRolls(List<(int, int)> coordinates, char[][] grid)
+    {
+        for(int i = 0; i < coordinates.Count; i++)
+        {
+            grid[coordinates[i].Item1][coordinates[i].Item2] = '.';
+        }
+
+        return grid;
+    }
+
+    private static char[][] GetInputFile()
+    {
+        string path = "Day4/input4.txt";
+        string[] inputLines = File.ReadAllLines(path);
+        char[][] grid = new char[inputLines.Length][];
+
+        for(int row = 0; row < inputLines.Length; row++)
+        {
+            grid[row] = inputLines[row].ToCharArray();
+        }
+
+        return grid;
+    }
+
+
+    public static void AccessibleRollsPart2()
+    {
+        bool rollsRemoved = true;
+        char[][] grid = GetInputFile();
+        int finalCount = 0;
+        
+        while(rollsRemoved == true)
+        {
+            List<(int,int)> coordinates = ToRemove(grid);
+            if(coordinates.Count == 0)
+            {
+                //Console.WriteLine(finalCount);
+                rollsRemoved = false;
+            }
+            else
+            {
+                finalCount += coordinates.Count;
+                RemoveRolls(coordinates, grid);
+            }
+        }
+        Console.WriteLine(finalCount);
+    }
 }
+
+// mark all acessible rolls as true using existing function. Return list of coordinates to remove - ToRemove()
+//create a second function that removes these coordinates taking in the list - Remove()
